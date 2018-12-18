a1762812e2e50fb89450a6b48693538c6753f206
lasserro <robert-lasser@hotmail.com>
2018-12-18 13:14:41 +0100

    one script to rule them all

---
+++ reports/IRL/Scripts_IRL/Master_script.R
@@ -0,0 +1,20 @@
+########### ############################################### ####################
+#######################MASTER SCRIPT############ ###############################
+## ########## ########################## ######################### #############
+######"This is why we wipe the floor with those stata guys."############### ####
+
+library(eurostat)
+library(dplyr)
+library(survey)
+library(convey)
+
+country <- "IE"
+year <- seq(2004, 2013, 1) # keep that period. 2014-2016 is included seperately
+
+### Data collection
+### make sure to use eduroam or WU VPN
+source('./reports/IRL/Scripts_IRL/_connection.R')
+
+
+### Data transformation
+source('./reports/IRL/Scripts_IRL/_setup_IRL.R')
