11b1f82ecfed3244dad14ef90358b88c4e836054
lasserro <robert-lasser@hotmail.com>
2019-01-08 17:35:13 +0100

    minor change

--- reports/IRL/Scripts_IRL/youth_unemployment.R
+++ reports/IRL/Scripts_IRL/youth_unemployment.R
@@ -5,8 +5,8 @@ es_toc <- get_eurostat_toc()
 
 # tipslm80 = unemployment rate of people aged 15 - 24 as a percentage of the 
 # labour force of the same age. 
-ur_IRL <- get_eurostat('tipslm80', time_format = 'raw', geo = 'IE' )
-ur_IRL <- ur_IRL %>% filter(geo == 'IE' )
+yunempl <- get_eurostat('tipslm80', time_format = 'raw', geo = 'IE' )
+yunempl <- yunempl %>% filter(geo == 'IE' )
 
 # tespm080 = percentage of unemployed young people (i.e. people aged 15-24) in 
 # the total population of this age group.
@@ -15,6 +15,9 @@ yur_ratio <- yur_ratio %>% filter( geo == 'IE')
 
 # ich glaube wir sollten mit tipslm80 arbeiten
 
-yunempl <- ur_IRL %>% filter(unit == 'PC_ACT')
+yunempl <- yunempl %>% filter(unit == 'PC_ACT')
 plot(yunempl$time, yunempl$values, type = 'l')
 
+yunempl_0416 <- yunempl %>% filter(time >= 2004 & time < 2017)
+write.csv(yunempl, file = './reports/IRL/tables/youth_unemployment_full.csv')
+write.csv(yunempl_0416, file = './reports/IRL/tables/youth_unemployment_0416.csv')
