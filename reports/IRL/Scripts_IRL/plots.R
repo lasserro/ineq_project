4145ba3491c79c9586d7514694eec6530fe54969
lasserro <robert-lasser@hotmail.com>
2019-01-12 18:49:32 +0100

    added plots

--- reports/IRL/Scripts_IRL/plots.R
+++ reports/IRL/Scripts_IRL/plots.R
@@ -105,6 +105,27 @@ p8020_plot_p1 <- ggplot() +
   theme(legend.position="bottom")
 p8020_plot_p1
 
+######## Plot P90/P10 Eurostat #################
+
+p9010_plot_p1 <- ggplot() +
+  geom_line(mapping = aes(y = indicators_p11$P90.P10, x = indicators_p11$Jahr,
+                          color = "Pre-tax factor income"),
+            size = 1 ) +
+  geom_line(mapping = aes(y = indicators_p12$P90.P10,x = indicators_p12$Jahr,
+                          color = "Pre-tax national income"), size = 1) +
+  geom_line(mapping = aes(y = indicators_p13$P90.P10,x = indicators_p13$Jahr,
+                          color = "Post-tax disposable income"), 
+            size = 1) + scale_color_manual(values = 
+                                             c('Pre-tax factor income' = 'red', 
+                                               'Pre-tax national income' = 'darkblue', 
+                                               'Post-tax disposable income' = 'darkgreen')) +
+  labs(color = '', x = "Year", y = "ratio", 
+       title = "P90/P10 ratio",
+       subtitle = "equal sharing of resources within household", 
+       caption = 'Source: Authors\' analysis (EU-SILC)') +
+  theme_linedraw() + scale_x_continuous(breaks = brks) +
+  theme(legend.position="bottom")
+p9010_plot_p1
 
 ######## Plot Top 10% share Eurostat #################
 
@@ -128,6 +149,26 @@ top10_plot_p1 <- ggplot() +
   theme(legend.position="bottom")
 top10_plot_p1
 
+######### Plot poverty rate Eurostat ############
+poverty_plot_p1 <- ggplot() +
+  geom_line(mapping = aes(y = poverty_p1$`Pre-tax factor income`, x = poverty_p1$Year,
+                          color = "Pre-tax factor income"),
+            size = 1 ) +
+  geom_line(mapping = aes(y = poverty_p1$`Pre-tax national income`, x = poverty_p1$Year,
+                          color = "Pre-tax national income"), size = 1) +
+  geom_line(mapping = aes(y = poverty_p1$`Post-tax disposable income`, x = poverty_p1$Year,
+                          color = "Post-tax disposable income"), 
+            size = 1) + scale_color_manual(values = 
+                                             c('Pre-tax factor income' = 'red', 
+                                               'Pre-tax national income' = 'darkblue', 
+                                               'Post-tax disposable income' = 'darkgreen')) +
+  labs(color = '', x = "Year", y = "Poverty rate in %", 
+       title = "At-risk-of-poverty rate",
+       subtitle = "equal sharing of resources within household", 
+       caption = 'Source: Authors\' analysis (EU-SILC)') +
+  theme_linedraw() + scale_x_continuous(breaks = brks) +
+  theme(legend.position="bottom")
+poverty_plot_p1
 
 
 ############################################################################
@@ -194,7 +235,7 @@ gini_plot_p2 <- ggplot() +
   labs(color = '', x = "Year", y = "Gini Index", 
        title = "Gini Index",
        subtitle = "partial sharing of resources", 
-       caption = 'Source: Authors\' analysis (EU-SILC)') +
+       caption = ' ') +
   theme_linedraw() + scale_x_continuous(breaks = brks) +
   theme(legend.position="bottom")
 gini_plot_p2
@@ -217,11 +258,32 @@ p8020_plot_p2 <- ggplot() +
   labs(color = '', x = "Year", y = "ratio", 
        title = "P80/P20 ratio",
        subtitle = "partial sharing of resources", 
-       caption = 'Source: Authors\' analysis (EU-SILC)') +
+       caption = ' ') +
   theme_linedraw() + scale_x_continuous(breaks = brks) +
   theme(legend.position="bottom")
 p8020_plot_p2
 
+######## Plot P90/P10 wid #################
+
+p9010_plot_p2 <- ggplot() +
+  geom_line(mapping = aes(y = indicators_p21$P90.P10, x = indicators_p21$Jahr,
+                          color = "Pre-tax factor income"),
+            size = 1 ) +
+  geom_line(mapping = aes(y = indicators_p22$P90.P10,x = indicators_p22$Jahr,
+                          color = "Pre-tax national income"), size = 1) +
+  geom_line(mapping = aes(y = indicators_p23$P90.P10,x = indicators_p23$Jahr,
+                          color = "Post-tax disposable income"), 
+            size = 1) + scale_color_manual(values = 
+                                             c('Pre-tax factor income' = 'red', 
+                                               'Pre-tax national income' = 'darkblue', 
+                                               'Post-tax disposable income' = 'darkgreen')) +
+  labs(color = '', x = "Year", y = "ratio", 
+       title = "P90/P10 ratio",
+       subtitle = "partial sharing of resources", 
+       caption = 'Source: Authors\' analysis (EU-SILC)') +
+  theme_linedraw() + scale_x_continuous(breaks = brks) +
+  theme(legend.position="bottom")
+p9010_plot_p2
 
 ######## Plot Top 10% share Wid #################
 
@@ -240,10 +302,33 @@ top10_plot_p2 <- ggplot() +
   labs(color = '', x = "Year", y = "income share in %", 
        title = "Income share of Top Decile",
        subtitle = "partial sharing of resources", 
-       caption = 'Source: Authors\' analysis (EU-SILC)') +
+       caption = ' ') +
   theme_linedraw() + scale_x_continuous(breaks = brks) +
   theme(legend.position="bottom")
 top10_plot_p2
+
+######### Plot poverty rate wid ############
+poverty_plot_p2 <- ggplot() +
+  geom_line(mapping = aes(y = poverty_p2$`Pre-tax factor income`, x = poverty_p2$Year,
+                          color = "Pre-tax factor income"),
+            size = 1 ) +
+  geom_line(mapping = aes(y = poverty_p2$`Pre-tax national income`, x = poverty_p2$Year,
+                          color = "Pre-tax national income"), size = 1) +
+  geom_line(mapping = aes(y = poverty_p2$`Post-tax disposable income`, x = poverty_p2$Year,
+                          color = "Post-tax disposable income"), 
+            size = 1) + scale_color_manual(values = 
+                                             c('Pre-tax factor income' = 'red', 
+                                               'Pre-tax national income' = 'darkblue', 
+                                               'Post-tax disposable income' = 'darkgreen')) +
+  labs(color = '', x = "Year", y = "Poverty rate in %", 
+       title = "Poverty rate",
+       subtitle = "partial sharing of resources", 
+       caption = ' ') +
+  theme_linedraw() + scale_x_continuous(breaks = brks) +
+  theme(legend.position="bottom")
+poverty_plot_p2
+
+
 
 
 ################# Plot youth unemployment #####################################
